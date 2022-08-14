68ec00a0331be3faac0d8fdc9b44d68eb53ee1ef
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-15 00:28:59 +0530

    ✅ Default parameters

---
+++ 7-default-params.js
@@ -0,0 +1,8 @@
+const print =(name="default")=>{
+    console.log(name);
+}
+print();
+print("Pavitra");
+
+//if we want to destructure an object but if object doesn't exists the we 
+//destructure an empty object like function fun(data,{lable}={}){}
